b47a192c6dd1a9f36855bba938627f90b019e98c
Cir1lo <avison.ac.cirilo@gmail.com>
2023-07-12 13:10:33 -0300

    Conta.Banco Update

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int Numero;
-        String Agencia;
-        String nomeCliente;
-        double Saldo;
+        int Numero=0;
+        String Agencia="";
+        String nomeCliente="";
+        double Saldo=0;
 
         //Metodos 
-        System.out.println("Digite o numero da Agencia!");
+        
+        System.out.println("Nome do Cliente");
+        nomeCliente = input.next();
+        System.out.println("Digite o numero da conta: ");
         Numero = input.nextInt();
         System.out.println("Qual a agencia?");
         Agencia = input.next();
-        System.out.println("Nome do Cliente");
+        System.out.println("Qual o seu saldo?");
         Saldo = input.nextDouble();
 
         //Retorno
-        System.out.println("Ola"++", obrigado por criar uma conta em nosso banco, sua agencia e "+Agencia+", conta [Numero] e seu saldo [Saldo] já está disponível para saque");
+        System.out.println("\nOla"+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agencia e "+Agencia+", conta "+Numero+" e seu saldo "+Saldo+" já está disponível para saque\n");
 
     }
 }
